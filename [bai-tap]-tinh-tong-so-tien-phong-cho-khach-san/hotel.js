bbe2327cb8e32a0e6a52efa7a8b9e038cc028142
My.Project <khuong.kyuubi@gmail.com>
2022-05-13 13:09:36 +0700

    Thêm trường hợp

--- [bai-tap]-tinh-tong-so-tien-phong-cho-khach-san/hotel.js
+++ [bai-tap]-tinh-tong-so-tien-phong-cho-khach-san/hotel.js
@@ -1,13 +1,32 @@
-var matrix = [[0, 1, 1, 2],
-    [0, 5, 0, 0],
+// let matrix: number[][] = [[0, 1, 1, 2],
+//                           [0, 5, 0, 0],
+//                           [2, 0, 3, 3]];
+//
+// const matrixElementsSum = matrix => {
+//     let sum: number = 0;
+//     // Duyệt theo chiều dọc, gặp số 0 là break luôn
+//     for (let i = 0; i < matrix[0].length; i++) {
+//         for (let j = 0; j < matrix.length; j++) {
+//             if (matrix[j][i] === 0) {
+//                 break;
+//             }
+//             sum += matrix[j][i];
+//         }
+//     }
+//     return sum;
+// }
+//
+// console.log(matrixElementsSum(matrix)); // 7
+var matrix = [[0, 1, 0, 0],
+    [0, 5, 3, 2],
     [2, 0, 3, 3]];
 var matrixElementsSum = function (matrix) {
     var sum = 0;
     // Duyệt theo chiều dọc, gặp số 0 là break luôn
     for (var i = 0; i < matrix[0].length; i++) {
         for (var j = 0; j < matrix.length; j++) {
-            if (matrix[j][i] === 0) {
-                break;
+            if ( /*matrix[j][i] === 0 || */(j > 0 && matrix[j - 1][i] === 0)) {
+                continue;
             }
             sum += matrix[j][i];
         }
--- [bai-tap]-tinh-tong-so-tien-phong-cho-khach-san/hotel.ts
+++ [bai-tap]-tinh-tong-so-tien-phong-cho-khach-san/hotel.ts
@@ -1,5 +1,26 @@
-let matrix: number[][] = [[0, 1, 1, 2],
-                          [0, 5, 0, 0],
+// let matrix: number[][] = [[0, 1, 1, 2],
+//                           [0, 5, 0, 0],
+//                           [2, 0, 3, 3]];
+//
+// const matrixElementsSum = matrix => {
+//     let sum: number = 0;
+//     // Duyệt theo chiều dọc, gặp số 0 là break luôn
+//     for (let i = 0; i < matrix[0].length; i++) {
+//         for (let j = 0; j < matrix.length; j++) {
+//             if (matrix[j][i] === 0) {
+//                 break;
+//             }
+//             sum += matrix[j][i];
+//         }
+//     }
+//     return sum;
+// }
+//
+// console.log(matrixElementsSum(matrix)); // 7
+
+
+let matrix: number[][] = [[0, 1, 0, 0],
+                          [0, 5, 3, 2],
                           [2, 0, 3, 3]];
 
 const matrixElementsSum = matrix => {
@@ -7,8 +28,8 @@ const matrixElementsSum = matrix => {
     // Duyệt theo chiều dọc, gặp số 0 là break luôn
     for (let i = 0; i < matrix[0].length; i++) {
         for (let j = 0; j < matrix.length; j++) {
-            if (matrix[j][i] === 0) {
-                break;
+            if (/*matrix[j][i] === 0 || */(j > 0 && matrix[j - 1][i] === 0)) {
+                continue;
             }
             sum += matrix[j][i];
         }
